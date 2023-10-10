e49294075628d40a7e7f443a469fe02730f21025
Timofey Boyko <timofey.boyko@onlyoffice.com>
2023-10-10 17:09:16 +0300

    Package: add documentation for IPlugin interface

--- src/interfaces/plugins/IPlugin.ts
+++ src/interfaces/plugins/IPlugin.ts
@@ -1,13 +1,38 @@
 import { PluginStatus } from "../../enums";
-
+/**
+ * The default plugin.
+ *
+ * This interface must be implemented in every plugin, because without status it will not be built in.
+ */
 export interface IPlugin {
+  /**
+   * Stores the plugin status (active or hide).
+   */
   status: PluginStatus;
 
+  /**
+   * Stores callback which will be executed when uploading the plugin to the portal.
+   */
   onLoadCallback: () => Promise<void>;
 
+  /**
+   * Update the plugin status.
+   *
+   * @param status is a new plugin status.
+   */
   updateStatus(status: PluginStatus): void;
 
+  /**
+   * Get current plugin status
+   *
+   * @returns status
+   */
   getStatus(): PluginStatus;
 
+  /**
+   * Sets the onLoadCallback variable to the plugin.
+   *
+   * @param callback defines callback which will be executed when uploading the plugin to the portal.
+   */
   setOnLoadCallback(callback: () => Promise<void>): void;
 }
