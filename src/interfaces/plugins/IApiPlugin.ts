a6d01b89064e73fa3535a8d358e2de5be5e0da08
ovchinnikova-natalya <ovchinnikova.natalya@onlyoffice.com>
2023-10-13 18:45:01 +0300

    added some docs

--- src/interfaces/plugins/IApiPlugin.ts
+++ src/interfaces/plugins/IApiPlugin.ts
@@ -1,17 +1,77 @@
+/**
+ * The plugin that is provided with the origin, proxy, and prefix to make requests to the portal server.
+ */
 export interface IApiPlugin {
+  /**
+   * Stores the origin parameter of the DocSpace portal.
+   */
   origin: string;
+
+  /**
+   * Stores the proxy parameter of the DocSpace portal.
+   */
   proxy: string;
+
+  /**
+   * Stores the prefix parameter of the DocSpace portal to access the server side.
+   */
   prefix: string;
 
+  /**
+   * Update the origin parameter of the DocSpace portal.
+   *
+   * @param origin Defines the origin parameter of the DocSpace portal.
+   */
   setOrigin(origin: string): void;
+
+  /**
+   * Update the proxy parameter of the DocSpace portal.
+   *
+   * @param proxy Defines the proxy parameter of the DocSpace portal.
+   */
   setProxy(proxy: string): void;
+
+  /**
+   * Update the prefix parameter of the DocSpace portal.
+   *
+   * @param prefix Defines the prefix parameter of the DocSpace portal.
+   */
   setPrefix(prefix: string): void;
 
+  /**
+   * Get the origin parameter of the DocSpace portal.
+   *
+   * @returns The origin parameter of the DocSpace portal.
+   */
   getOrigin(): string;
+
+  /**
+   * Get the proxy parameter of the DocSpace portal.
+   *
+   * @returns The proxy parameter of the DocSpace portal.
+   */
   getProxy(): string;
+
+  /**
+   * Get the prefix parameter of the DocSpace portal.
+   *
+   * @returns The prefix parameter of the DocSpace portal to access the server side.
+   */
   getPrefix(): string;
 
+  /**
+   * Update all the API parameters of the DocSpace portal in one request.
+   *
+   * @param origin Defines the origin parameter of the DocSpace portal.
+   * @param proxy Defines the proxy parameter of the DocSpace portal.
+   * @param prefix Defines the prefix parameter of the DocSpace portal.
+   */
   setAPI(origin: string, proxy: string, prefix: string): void;
 
+  /**
+   * Get all the API parameters of the DocSpace portal in one request.
+   *
+   * @returns An object with the origin, proxy, and prefix parameters.
+   */
   getAPI(): { origin: string; proxy: string; prefix: string };
 }
--- src/interfaces/plugins/IPlugin.ts
+++ src/interfaces/plugins/IPlugin.ts
@@ -2,7 +2,7 @@ import { PluginStatus } from "../../enums";
 /**
  * The default plugin.
  *
- * This interface must be implemented in every plugin, because without status it will not be built in.
+ * This interface must be implemented in each plugin because without the plugin status it will not be built in.
  */
 export interface IPlugin {
   /**
@@ -18,21 +18,21 @@ export interface IPlugin {
   /**
    * Update the plugin status.
    *
-   * @param status is a new plugin status.
+   * @param status Defines a new plugin status (active or hide).
    */
   updateStatus(status: PluginStatus): void;
 
   /**
-   * Get current plugin status
+   * Get the current plugin status.
    *
-   * @returns status
+   * @returns The current plugin status.
    */
   getStatus(): PluginStatus;
 
   /**
    * Sets the onLoadCallback variable to the plugin.
    *
-   * @param callback defines callback which will be executed when uploading the plugin to the portal.
+   * @param callback Defines callback which will be executed when uploading the plugin to the portal.
    */
   setOnLoadCallback(callback: () => Promise<void>): void;
 }
