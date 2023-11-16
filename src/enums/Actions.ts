0cba7232c834009de35c796ab9b308450a1860b5
Timofey Boyko <timofey.boyko@onlyoffice.com>
2023-11-16 16:07:03 +0300

    SDK: add new interfaces and methods for saving admin settings to docspace

--- src/enums/Actions.ts
+++ src/enums/Actions.ts
@@ -1,18 +1,18 @@
 /*
-* (c) Copyright Ascensio System SIA 2023
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * (c) Copyright Ascensio System SIA 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
  * A collection of events that will be processed on the portal side.
@@ -96,4 +96,6 @@ export const enum Actions {
    * It does not work if the "postMessage" parameter is not passed to the message or the specified frame is not found.
    */
   sendPostMessage = "send-post-message",
+
+  saveSettings = "save-settings",
 }
--- src/interfaces/plugins/ISettingsPlugin.ts
+++ src/interfaces/plugins/ISettingsPlugin.ts
@@ -1,18 +1,18 @@
 /*
-* (c) Copyright Ascensio System SIA 2023
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * (c) Copyright Ascensio System SIA 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import { ISettings } from "../settings/ISettings";
 /**
@@ -36,6 +36,8 @@ export interface ISettingsPlugin {
    * @param settings Defines the administrator or owner settings block that is embedded in the modal window with the plugin description.
    */
   setAdminPluginSettings(settings: ISettings | null): void;
+
+  setAdminPluginSettingsValue(settings: string | null): void;
 
   /**
    * Get the administrator or owner plugin settings.
