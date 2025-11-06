a5d7c110fec6bf6347a3b0b8d3307b3b1ba5a0dd
Alexander Evdokimov <aleksandr.evdokimov@onlyoffice.com>
2025-11-06 18:22:06 +0300

    Interfaces: Components: add className prop

--- src/interfaces/components/ICheckbox.ts
+++ src/interfaces/components/ICheckbox.ts
@@ -145,4 +145,12 @@ export interface ICheckbox {
    * @category Appearance
    */
   title?: string;
+
+  /**
+   * Defines the CSS class for styling the component.
+   * Can be used to override or extend the default component styles.
+   *
+   * @category Appearance
+   */
+  className?: string;
 }
