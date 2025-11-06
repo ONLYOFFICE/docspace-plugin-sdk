9c01084317f977a04eec44503ec990b0fe55a630
Alexander Evdokimov <aleksandr.evdokimov@onlyoffice.com>
2025-11-06 18:25:17 +0300

    template: add CSS processing and minification to webpack config

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
@@ -10,7 +12,19 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "plugin.css",
+    }),
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
